8045d9a3174296b7b9947cd3cbaf0316903a1330
Abigail Teague <ateague063@gmail.com>
2018-03-20 18:48:17 -0400

    Created a MailParser class to hold most of the program logic

---
+++ MailParser.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+
+namespace workschedule
+{
+    class MailParser
+    {
+        private readonly Regex _regex;
+
+        private readonly UserCredential _credential;
+
+        private readonly CalendarService _calendarService;
+
+        private readonly GmailService _mailService;
+
+        public string UserId { get; set; }
+
+        public string CalendarId { get; set; }
+
+        public string MessageQuery { get; set; }
+
+        public string DateFormat { get; set; }
+
+        public string Summary { get; set; }
+
+        public string ColorId { get; set; }
+
+        public TextWriter Log { private get; set; }
+
+        public MailParser(string applicationName, Regex regex, UserCredential credential)
+        {
+            _regex = regex;
+            _credential = credential;
+            _calendarService = new CalendarService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = applicationName
+            });
+            _mailService = new GmailService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = applicationName
+            });
+
+            UserId = "me";
+            CalendarId = "primary";
+            Summary = "Work";
+            ColorId = "1";
+        }
+
+        public bool PopulateCalendar()
+        {
+            var msgResponse = new UsersResource.MessagesResource.ListRequest(_mailService, UserId)
+            {
+                Q = MessageQuery, //"from:WorkSchedules@dillards.com",
+                LabelIds = "INBOX"
+            }.Execute();
+
+            Message msg;
+            try
+            {
+                msg = msgResponse.Messages.Single();
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            //Get the body of the message as a string and find all matches
+            var body = MailParser.ConvertPayloadBody(_mailService.Users.Messages.Get(UserId, msg.Id).Execute());
+            var matches = _regex.Matches(body);
+
+            //Insert a new calendar entry for each match
+            foreach (Match match in matches)
+                _calendarService.Events.Insert(ParseMatch(match), CalendarId).Execute();
+
+            //Remove the inbox label from the message when done so that it isn't seen next time the program runs.
+            //This has the same effect as archiving the message in gmail.
+            _mailService.Users.Messages.Modify(new ModifyMessageRequest
+            {
+                RemoveLabelIds = {"INBOX"}
+            }, UserId, msg.Id);
+            return true;
+        }
+
+        private Event ParseMatch(Match match)
+        {
+            DateTime.TryParseExact($"{match.Groups["date"]} {match.Groups["in"]}", DateFormat, null,
+                DateTimeStyles.None, out var start);
+            DateTime.TryParseExact($"{match.Groups["date"]} {match.Groups["out"]}", DateFormat, null,
+                DateTimeStyles.None, out var end);
+
+            if (DateTime.Now.Month == 12 && start.Month == 1)
+            {
+                start = start.AddYears(1);
+                end = end.AddYears(1);
+            }
+
+            return new Event
+            {
+                Summary = Summary,
+                ColorId = ColorId,
+                Start = new EventDateTime
+                {
+                    DateTime = start
+                },
+                End = new EventDateTime
+                {
+                    DateTime = end
+                }
+            };
+        }
+
+        private static string ConvertPayloadBody(Message msg)
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(msg.Payload.Body
+                .Data.Replace('-', '+')));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -19,13 +19,12 @@ namespace workschedule
         private static readonly string[] Scopes = {CalendarService.Scope.Calendar, GmailService.Scope.GmailModify};
 
         private static readonly Regex Regex = new Regex(
-            @"(\w{3})\s*(?<date>\d{2}\/\d{2}): (?<in>\d{2}:\d{2}\w)(?:.*?)(?<out>\d{2}:\d{2}\w) \w{3}\d{3}\s?\r",
+            @"(?<date>\d{2}\/\d{2}): (?<in>\d{2}:\d{2}\w)(?:.*?)(?<out>\d{2}:\d{2}\w)(?= \w{3}\d{3}\s?\r)",
             RegexOptions.Compiled);
 
         private static void Main()
         {
             UserCredential credential;
-
             using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
             {
                 var credPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
@@ -34,67 +33,16 @@ namespace workschedule
                     Scopes, "user", CancellationToken.None, new FileDataStore(credPath, true)).Result;
                 Console.WriteLine($"Credential file saved to: {credPath}");
             }
-
-            var calendarService = new CalendarService(new BaseClientService.Initializer
+            var parser = new MailParser(ApplicationName, Regex, credential)
             {
-                HttpClientInitializer = credential,
-                ApplicationName = ApplicationName
-            });
-
-            var mailService = new GmailService(new BaseClientService.Initializer
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = ApplicationName
-            });
-
-            var msgRequest = new UsersResource.MessagesResource.ListRequest(mailService, "me")
-            {
-                Q = "from:WorkSchedules@dillards.com",
-                LabelIds = "INBOX"
+                DateFormat = "MM/dd hh:mmt",
+                MessageQuery = "from:WorkSchedules@dillards.com",
+                ColorId = "2"
             };
-
-            //Execute the message request, make sure there's exactly one viable message to work with.
-            var msgResponse = msgRequest.Execute().Messages;
-            if (msgResponse == null || msgResponse.Count > 1) return;
-            var msg = msgResponse[0];
-
-            var body = ConvertPayloadBody(mailService.Users.Messages.Get("me", msg.Id).Execute().Payload.Body.Data);
-            if (body.StartsWith("JANUARY")) return; //No code to handle this right now, so exit the program.
-            var year = body.Substring(body.IndexOf(' ') + 1, 4);
-            var matches = Regex.Matches(body);
-            Console.WriteLine("Populating calendar...");
-
-            foreach (Match matchItem in matches)
-            {
-                var calEvent = calendarService.Events.Insert(new Event
-                {
-                    Summary = "Work",
-                    ColorId = "2",
-                    Start = new EventDateTime
-                    {
-                        DateTime = DateTime.Parse($"{matchItem.Groups["date"]}/{year} {matchItem.Groups["in"]}M")
-                    },
-                    End = new EventDateTime
-                    {
-                        DateTime = DateTime.Parse($"{matchItem.Groups["date"]}/{year} {matchItem.Groups["out"]}M")
-                    }
-                }, "primary").Execute();
-                Console.WriteLine($"Created work entry on {calEvent.Start.DateTime?.ToShortDateString()}");
-            }
-
-            //Remove the inbox label from the message when done so that it isn't seen next time the program runs.
-            //This has the same effect as archiving the message in gmail.
-            mailService.Users.Messages.Modify(new ModifyMessageRequest
-            {
-                RemoveLabelIds = new[] {"INBOX"}
-            }, "me", msg.Id).Execute();
-            Console.WriteLine($"Done! Created {matches.Count} events.");
+            var s = "03/20: 12:11P";
+            var d = DateTime.ParseExact(s, "MM/dd: hh:mmt", null);
+            Console.WriteLine(d);
             Console.Read();
-        }
-
-        private static string ConvertPayloadBody(string data)
-        {
-            return Encoding.UTF8.GetString(Convert.FromBase64String(data.Replace('-', '+')));
         }
     }
 }
