2bf2630c7072a951aa0a594a8f60ce22da54b40e
Abigail Teague <ateague063@gmail.com>
2018-04-08 05:05:58 -0400

    More fault tolerance: The program can now take multiple messages at a time, and won't create duplicate events.

--- Log.cs
+++ Log.cs
@@ -17,19 +17,24 @@ namespace workschedule
         public Log(TextWriter writer)
         {
             _writer = writer;
-            _writer.WriteLine($"Begin log {DateTime.Now:G}");
+            _writer.WriteLine(
+                $"Begin log {DateTime.Now:G}");
         }
 
-
-        public async void WriteAsync(EventType eventType, string first, params string[] s)
+        public async void WriteAsync(EventType eventType,
+            string first, params string[] s)
         {
-            await WriteInternalAsync($"[{DateTime.Now:T}]", $"{eventType,-5:G} - {first}");
-            foreach (var str in s) await WriteInternalAsync(str);
+            await WriteInternalAsync($"[{DateTime.Now:T}]",
+                $"{eventType,-5:G} - {first}");
+            foreach (var str in s)
+                await WriteInternalAsync(str);
         }
 
-        private async Task WriteInternalAsync(string head, string tail)
+        private async Task WriteInternalAsync(string head,
+            string tail)
         {
-            await _writer.WriteLineAsync($"{head,12} {tail}");
+            await _writer.WriteLineAsync(
+                $"{head,12} {tail}");
         }
 
         private async Task WriteInternalAsync(string s)
--- Program.cs
+++ Program.cs
@@ -1,69 +1,101 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
-using System.Threading;
 using System.Threading.Tasks;
-using Google.Apis.Auth.OAuth2;
 using Google.Apis.Calendar.v3;
 using Google.Apis.Gmail.v1;
-using Google.Apis.Util.Store;
+using Google.Apis.Gmail.v1.Data;
 
 namespace workschedule
 {
     internal static class Program
     {
-        private const string ApplicationName = "Dillard's Calendar Import";
-        private static readonly string[] Scopes = {CalendarService.Scope.Calendar, GmailService.Scope.GmailModify};
-        private static readonly Log Log = new Log(Console.Out);
+        private const string ApplicationName =
+            "Dillard's Calendar Import";
+
+        private static readonly string[] Scopes =
+        {
+            CalendarService.Scope.Calendar,
+            GmailService.Scope.GmailModify
+        };
+
+        private static readonly Log Log =
+            new Log(Console.Out);
 
         private static readonly Regex Regex = new Regex(
             @"(?<date>\d{2}\/\d{2}): (?<in>\d{2}:\d{2}\w)(?:.*?)(?<out>\d{2}:\d{2}\w)(?= \w{3}\d{3}\s?\r)",
             RegexOptions.Compiled);
 
-
         private static async Task Main(string[] args)
         {
             var credPath = Path.Combine(
-                Environment.GetFolderPath(Environment.SpecialFolder.Personal),
+                Environment.GetFolderPath(Environment
+                    .SpecialFolder.Personal),
                 $".credentials/{ApplicationName}.json");
             var isTestRun = false;
             var reauthorize = false;
+            IEnumerable<Message> messages;
             for (var i = 0; i < args.Length; i++)
                 switch (args[i])
                 {
                     case "-a":
                         reauthorize = true;
                         break;
+
                     case "-t":
                         isTestRun = true;
-                        Log.WriteAsync(Log.EventType.Info, "TEST RUN");
+                        Log.WriteAsync(Log.EventType.Info,
+                            "TEST RUN");
                         break;
+
                     case "-p":
-                        credPath = Path.GetFullPath(args[++i]);
+                        credPath =
+                            Path.GetFullPath(args[++i]);
                         break;
                 }
-            var parser = new MailParser(ApplicationName, Regex, await GetCredentials(credPath, reauthorize))
+            if (reauthorize)
+                Directory.Delete(credPath, true);
+            var serviceprovider =
+                new ServiceProvider(ApplicationName,
+                    credPath, Scopes)
+                {
+                    Log = Log,
+                    MessageQuery =
+                        "from:WorkSchedules@dillards.com"
+                };
+
+            var parser = new MailParser(Regex)
             {
                 DateFormat = "MM/dd hh:mmt",
-                MessageQuery = "from:WorkSchedules@dillards.com",
                 ColorId = "2",
                 Log = Log
             };
-            await parser.PopulateCalendarAsync();
-            if (isTestRun) await parser.EraseEvents();
-        }
 
-        private static async Task<UserCredential> GetCredentials(string path, bool reauthorize = false)
-        {
-            if (reauthorize && Directory.Exists(path)) Directory.Delete(path, true);
-            UserCredential credential;
-            using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+
+            try
             {
-                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(GoogleClientSecrets.Load(stream).Secrets,
-                    Scopes, "user", CancellationToken.None, new FileDataStore(path, true));
+                messages =
+                    await serviceprovider.GetMessageAsync(
+                        !isTestRun);
+            }
+            catch (InvalidOperationException)
+            {
+                Log.WriteAsync(Log.EventType.Info,
+                    "Exiting program.");
+                return;
             }
 
-            return credential;
+            var insertTask = Task.WhenAll(messages
+                .SelectMany(parser.ParseMessage)
+                .Select(serviceprovider.InsertEventAsync));
+            await insertTask;
+            Log.WriteAsync(Log.EventType.Info,
+                "Finished creating events.");
+            if (isTestRun)
+                await serviceprovider.EraseEvents();
+            Console.Read();
         }
     }
 }
